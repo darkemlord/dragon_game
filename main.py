466a7765755e8a266029357e508698f9b81bc896
emanuel caro <darkemlord@gmail.com>
2024-02-23 21:29:41 +0900

    refs #3 adding text items

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ pg.init()
 
 # Display Surface
 
-WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 300
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 400
 display_surface = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pg.display.set_caption("Feed the dragon!")
 
@@ -14,18 +14,53 @@ pg.display.set_caption("Feed the dragon!")
 FPS = 60
 clock = pg.time.Clock()
 
+# Load game values
+PLAYER_STARTING_LIVES = 5
+PLAYER_VELOCITY = 5
+COIN_STARTING_VELOCITY = 5
+COIN_ACCELERATION = 0.5
+BUFFER_DISTANCE = 100
+
+score = 0
+player_lives = PLAYER_STARTING_LIVES
+coin_velocity = COIN_STARTING_VELOCITY
+
+# Set Colors
+GREEN = (0, 255, 0)
+DARK_GREEN = (10, 50, 10)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+# Set fonts
+font = pg.font.Font("./assets/AttackGraffiti.ttf", 32)
+
+
+# Set text
+
+score_text = font.render("Score: " + str(score), True, GREEN, DARK_GREEN)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 10)
+
+title_text = font.render("Feed the dragon", True, GREEN, WHITE)
+title_rect = title_text.get_rect()
+title_rect.centerx = WINDOW_WIDTH // 2
+title_rect.y = 10
+
+lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARK_GREEN)
+lives_rect = lives_text.get_rect()
+lives_rect.topright = (WINDOW_WIDTH, 0)
+
 # Load character images
 
 dragon_image = pg.image.load("./assets/dragon_right.png")
 dragon_rect = dragon_image.get_rect()
 dragon_rect.topleft = (25, 25)
 
-# Load sound effects
 
 coin_image = pg.image.load("./assets/coin.png")
 coin_rect = coin_image.get_rect()
 coin_rect.topright = (25, 25)
 
+# Load sound effects
 # Main loop game
 running = True
 
@@ -34,6 +69,13 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
+    # Display text items
+    display_surface.blit(score_text, score_rect)
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(lives_text, lives_rect)
+    display_surface.blit(score_text, score_rect)
+
+    # Display Characters
     display_surface.blit(dragon_image, dragon_rect)
     display_surface.blit(coin_image, coin_rect)
 
